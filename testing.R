f3556d39fd4717067dbd701ff313731b0ec717f2
canthonyscott <anthony@canthonyscott.com>
2016-06-03 00:11:44 -0500

    df declared before rbind

--- testing.R
+++ testing.R
@@ -5,6 +5,8 @@ newlist <- strsplit(list_of_genes, " ")
 
 rlog_data_all <- read.table("data/rld_all.tabular")
 
+df <- data.frame()
+
 for (i in 1:length(newlist)){
       df <- rbind(df, rlog_data_all[grepl(newlist[i], rownames(rlog_data_all)),])
   }
